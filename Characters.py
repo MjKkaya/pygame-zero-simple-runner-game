0f889ad570b7618d28417276ac9e4608f4ecfd2b
MjKkaya <mjkkaya@gmail.com>
2025-03-10 02:37:11 +0300

    feat: modified player and enemy images. Added background scrolling. Added in game sounds

--- Characters.py
+++ Characters.py
@@ -2,28 +2,23 @@ from pgzero.builtins import Actor
 
 
 class Character(Actor):
-    gravity = 1
-    default_jump_velocity = -15
 
-
-    def __init__(self, image, position, current_state, animations):
-        super().__init__(image, position)
+    def __init__(self, position, current_state, animations, fps):
+        super().__init__(animations[current_state][0], position)
         self.state = current_state              # default state
         self.animations = animations            # Store animation dictionary
         self.elapsedtime = 0
         self.imageIndexNo = 0
-        self.is_ground = True
-        self.jump_elapsed_time = 0
         self.default_pos = position
-        self.jump_velocity = 0
+        self.animation_time = 1/fps
+        #print("fps:"+str(self.animation_time))
+
 
 
     def sprite_animation(self, dt):
         self.elapsedtime += dt
-#        return
-        if self.elapsedtime > 0.03:  # fps is 33 frame
+        if self.elapsedtime > self.animation_time:
             self.elapsedtime = 0
-            #print("imageIndexNo:",self.imageIndexNo, "ss:",self.animations[self.state][self.imageIndexNo])
             self.image = self.animations[self.state][self.imageIndexNo]
             self.imageIndexNo += 1
             if self.imageIndexNo == len(self.animations[self.state])-1:
@@ -33,25 +28,53 @@ class Character(Actor):
     def update(self, dt):
         self.sprite_animation(dt)
 
-        if not self.is_ground:
-            self.y += self.jump_velocity
-            self.jump_velocity += self.gravity
 
-            print("y:",self.y)
-            if self.default_pos[1] == self.y:
-                self.jump_velocity = 0
+
+class Player(Character):
+    gravity = 1
+    jump_velocity = -20
+
+
+    def __init__(self, position, current_state, animations, fps = 30):
+        super().__init__(position, current_state, animations, fps)
+        self.is_ground = True
+        self.jump_elapsed_time = 0
+        self.current_jump_velocity = 0
+
+
+    def update(self, dt):
+        super().update(dt)
+        if not self.is_ground:  # player is in the air!
+            self.y += self.current_jump_velocity
+            self.current_jump_velocity += self.gravity
+
+            if self.default_pos[1] == self.y:   # player touchs the ground.
+                self.current_jump_velocity = 0
                 self.is_ground = True
                 self.state = "run"
 
 
-
     def jump(self):
-        print("is_ground:",self.is_ground)
         if self.is_ground:
-            self.jump_velocity = self.default_jump_velocity
+            self.current_jump_velocity = self.jump_velocity
             self.elapsedtime = 0
             self.imageIndexNo = 0
             self.jump_elapsed_time = 0
             self.state = "jump"
             self.is_ground = False
+
+
+
+class Enemy(Character):
+
+    def __init__(self, position, current_state, animations, fps = 30):
+        super().__init__(position, current_state, animations, fps)
+
+
+    def update(self, dt):
+        super().update(dt)
+
+
+    def move(self):
+        print("move")
 
--- Main.py
+++ Main.py
@@ -1,21 +1,27 @@
 import pgzrun
-from Characters import Character
-
+import random
+from Characters import *
+from InfiniteBackground import InfiniteBackground
 
 WIDTH = 900
 HEIGHT = 500
-
 
 game_state = 0              # 0: Game not started,  1: Game started, 2: Game over
 is_sound_on = True
 menu_button_size = (64, 64)
 
+score = 10;
+total_score = 0;
 
-# Begin region : Home Screen
 
-if is_sound_on:
-    music.play("intro")
+def play_music(name):
+    if is_sound_on:
+        music.play(name)
 
+
+# Region : Home Screen
+
+play_music("intro")
 play_button = Actor("ui/play")
 sound_icon_on = "ui/sound_on"
 sound_icon_off = "ui/sound_off"
@@ -47,10 +53,7 @@ def check_home_screen_mouse_inputs(button, pos):
     if button == mouse.LEFT:
         global is_sound_on
         if play_button.collidepoint(pos):
-            global game_state
-            game_state = 1
-            if is_sound_on:
-                music.play("game_music")
+            initialize_game_screen()
 
         elif sound_button.collidepoint(pos):
             is_sound_on = not is_sound_on
@@ -66,62 +69,112 @@ def check_home_screen_mouse_inputs(button, pos):
 # End region
 
 
-# Begin region : Game Screen
+# Region : Game Screen
 
-enemy_1_animations = {
-    "idle": ["knight_idle_0", "knight_idle_1", "knight_idle_2"],
-    "run": ["knight_run_0", "knight_run_1", "knight_run_2", "knight_run_3"]
-}
-
-enemy_2_animations = {
-    "idle": ["knight_idle_0", "knight_idle_1", "knight_idle_2"],
-    "run": ["knight_run_0", "knight_run_1", "knight_run_2", "knight_run_3"]
-}
-
-
+infinite_background = InfiniteBackground(["game_background", "game_background"])
+carrot = Actor("carrot")
 
 player_run_path = "characters/player/run/"
 player_jump_path = "characters/player/jump/"
-player_animations= {
+player_animations = {
     "run": [player_run_path+"run_0", player_run_path+"run_1", player_run_path+"run_2", player_run_path+"run_3", player_run_path+"run_4", player_run_path+"run_5", player_run_path+"run_6", player_run_path+"run_7"],
     "jump": [player_jump_path+"jump_0", player_jump_path+"jump_1", player_jump_path+"jump_2", player_jump_path+"jump_3", player_jump_path+"jump_4", player_jump_path+"jump_5", player_jump_path+"jump_6", player_jump_path+"jump_7"],
 }
-player = Character("characters/player/bunny", (200,370), "run", player_animations)
+player = Player((200,370), "run", player_animations)
+
 
 enemy_list = []
-enemy_count = 5
+active_enemy_list = []
+max_enemy_count = 10
 
+fly_enemy_path = "characters/enemies/fly/"
+fly_enemy_animations = { "fly": [fly_enemy_path+"fly_00", fly_enemy_path+"fly_01", fly_enemy_path+"fly_02", fly_enemy_path+"fly_03", fly_enemy_path+"fly_04", fly_enemy_path+"fly_05", fly_enemy_path+"fly_06", fly_enemy_path+"fly_07", fly_enemy_path+"fly_08", fly_enemy_path+"fly_09", fly_enemy_path+"fly_10"] }
+fly_enemy = Enemy((800,370), "fly", fly_enemy_animations)
+
+walk_enemy_path = "characters/enemies/walk/"
+walk_enemy_animations = { "walk": [walk_enemy_path+"walk_0", walk_enemy_path+"walk_1", walk_enemy_path+"walk_2", walk_enemy_path+"walk_3", walk_enemy_path+"walk_4", walk_enemy_path+"walk_5", walk_enemy_path+"walk_6", walk_enemy_path+"walk_7"] }
+walk_enemy = Enemy((400,370), "walk", walk_enemy_animations, 8)
+walk_enemy.bottom = player.bottom
+
+stand_enemy_path = "characters/enemies/stand/"
+stand_enemy_animations = { "stand": [stand_enemy_path+"stand_0", stand_enemy_path+"stand_1", stand_enemy_path+"stand_2", stand_enemy_path+"stand_3", stand_enemy_path+"stand_4", stand_enemy_path+"stand_5", stand_enemy_path+"stand_6", stand_enemy_path+"stand_7"] }
+stand_enemy = Enemy((600,370), "stand", stand_enemy_animations, 8)
+stand_enemy.bottom = player.bottom
+
+
+def set_carrot():
+    carrot.x = random.randrange(WIDTH + 100 , WIDTH + 500)
+    carrot.y = random.randrange(150, 400)
+    carrot.angle = random.randrange(360)
+
+
+def initialize_game_screen():
+    global game_state
+    game_state = 1
+    play_music("game_music")
+    infinite_background.set_active_scrolling(True)
+    global total_score
+    total_score = 0
+    set_carrot()
+
+
+def check_player_collider():
+    if player.colliderect(carrot):
+        set_carrot()
+        sounds.pickup.play()
+        global total_score
+        total_score += score
+
+
+def draw_game_background():
+    screen.fill((46,46,255))
+    infinite_background.draw()
 
 def draw_game_screen():
+    draw_game_background()
     player.draw()
+    fly_enemy.draw()
+    walk_enemy.draw()
+    stand_enemy.draw()
+    screen.draw.textbox(f"Score: {total_score}", (0, 10, 100, 20), color="white", owidth=1, ocolor="black")
+    carrot.draw()
+
+def update_game_screen(dt):
+    player.update(dt)
+    fly_enemy.update(dt)
+    walk_enemy.update(dt)
+    stand_enemy.update(dt)
+    check_player_collider()
+    carrot.x -= 300 * dt
+    if carrot.right < 0:
+        set_carrot()
 
 
 def check_game_screen_keyboard_inputs(key):
     if key == keys.RETURN:
         global game_state
         game_state = 2
+        infinite_background.set_active_scrolling(False)
         if is_sound_on:
             music.stop()
             sounds.game_over.play()
 
     if key == keys.SPACE:
         player.jump()
-
+        sounds.jump.play()
 
 
 # End region
 
 
-# Begin region : Game Over Screen
+# Region : Game Over Screen
 
 home_button = Actor("ui/home")
 
 def draw_game_over_screen():
     screen.blit("background", (0, 0))
     screen.draw.textbox("GAME OVER", (WIDTH*0.1, (HEIGHT * 0.25) ,WIDTH*0.8,65), color="white", owidth=1, ocolor="red")
-    screen.draw.textbox(f"Final Score: {99}", (WIDTH*0.1, (HEIGHT * 0.25) + 100, WIDTH*0.8, 35), color="white")
-    #screen.draw.textbox("Press 'Enter' to return to the menu.", (WIDTH*0.1, (HEIGHT * 0.25) +200 ,WIDTH*0.8,35), color="white")
-
+    screen.draw.textbox(f"Total Score: {total_score}", (WIDTH*0.1, (HEIGHT * 0.25) + 100, WIDTH*0.8, 35), color="white")
 
     button_pos_y = HEIGHT * 0.75
     play_button.midleft = ((WIDTH * 0.5) - (menu_button_size[0] * 2), button_pos_y)
@@ -137,20 +190,11 @@ def check_game_over_screen_mouse_inputs(button, pos):
 
         if home_button.collidepoint(pos):
             game_state = 0
-            if is_sound_on:
-                music.play("intro")
+            play_music("intro")
 
         if play_button.collidepoint(pos):
-            game_state = 1
-            if is_sound_on:
-                music.play("game_music")
+            initialize_game_screen()
 
-"""
-def check_game_over_screen_keyboard_inputs(key):
-    if key == keys.RETURN:
-        global game_state
-        game_state = 0
-"""
 # End region
 
 
@@ -166,7 +210,7 @@ def draw():
 
 def update(dt):
     if game_state == 1:
-        player.update(dt)
+        update_game_screen(dt)
 
 
 def on_mouse_down(button, pos):
@@ -180,6 +224,5 @@ def on_key_down(key):
         check_game_screen_keyboard_inputs(key)
     #elif game_state == 2:
     #    check_game_over_screen_keyboard_inputs(key)
-
 
 pgzrun.go()
